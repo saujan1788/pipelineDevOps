4181ac5dc0bd528d639f3366c3e46d8b7dae870a
saujan1788 <saujan1788@gmail.com>
2024-04-27 20:09:42 +0100

    Update ProductServiceApplicationTests.java

--- src/test/java/com/saujan/microservices/productservice/ProductServiceApplicationTests.java
+++ src/test/java/com/saujan/microservices/productservice/ProductServiceApplicationTests.java
@@ -1,9 +1,5 @@
 package com.saujan.microservices.productservice;
 
-import de.flapdoodle.embed.mongo.MongodExecutable;
-import de.flapdoodle.embed.mongo.MongodStarter;
-import de.flapdoodle.embed.mongo.config.MongodConfig;
-import de.flapdoodle.embed.mongo.distribution.Version;
 import io.restassured.RestAssured;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.AfterAll;
@@ -12,57 +8,28 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ProductServiceApplicationTests {
 
-    private static MongodExecutable mongodExecutable;
-    private static String bindIp = "localhost";
-    private static int port = 27017;
-
-    @BeforeAll
-    static void setup() throws Exception {
-        MongodStarter starter = MongodStarter.getDefaultInstance();
-        MongodConfig mongodConfig = MongodConfig.builder()
-            .version(Version.Main.PRODUCTION)
-            .build();
-
-        mongodExecutable = starter.prepare(mongodConfig);
-        mongodExecutable.start();
-    }
-
-    @AfterAll
-    static void cleanup() {
-        if (mongodExecutable != null) {
-            mongodExecutable.stop();
-        }
-    }
-
-    @DynamicPropertySource
-    static void mongoProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.data.mongodb.uri", () -> "mongodb://" + bindIp + ":" + port);
-    }
-
     @LocalServerPort
-    private int localPort;
+    private int port;
 
     @BeforeEach
-    void initialize() {
+    void setup() {
         RestAssured.baseURI = "http://localhost";
-        RestAssured.port = localPort;
+        RestAssured.port = port;
     }
 
     @Test
     void shouldCreateProduct() {
         String requestBody = """
-            {
-                "name": "iPhone_15",
-                "description": "iPhone 15 is a smartphone from Apple",
-                "price": 1000
-            }
-        """;
+                {
+                    "name": "iPhone_15",
+                    "description": "iPhone 15 is a smartphone from Apple",
+                    "price": 1000
+                }
+            """;
 
         RestAssured.given().contentType("application/json")
                 .body(requestBody)
